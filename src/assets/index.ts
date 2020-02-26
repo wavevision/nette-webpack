49c253e4c2832daeb4de8db285e9be515f67376f
Vít Rozsíval <mail@vitrozsival.cz>
2020-02-26 16:00:21 +0100

    Update dependecies and exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,3 @@
 export { default as Neon } from './Neon';
 export { default as WebpackHelper } from './WebpackHelper';
-export { NeonConfig, Options } from './WebpackHelper/types';
+export { Entries, NeonConfig, Options } from './WebpackHelper/types';
